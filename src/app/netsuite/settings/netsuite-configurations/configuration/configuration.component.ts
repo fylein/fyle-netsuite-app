4014a2cc3bd0811c76ca90e1b1ede3c408c6c2b2
ashwinthanaraj <ashwin.t@fyle.in>
2021-02-15 21:22:30 +0530

    toast message

--- src/app/netsuite/settings/netsuite-configurations/configuration/configuration.component.ts
+++ src/app/netsuite/settings/netsuite-configurations/configuration/configuration.component.ts
@@ -251,6 +251,11 @@ export class ConfigurationComponent implements OnInit {
         that.settingsService.postGeneralSettings(that.workspaceId, reimbursableExpensesObject, cccExpensesObject, fyleToNetSuite, netSuiteToFyle, importProjects, autoMapEmployees).subscribe(() => {
           that.isLoading = true;
           that.snackBar.open('Configuration saved successfully');
+          if (autoMapEmployees) {
+            setTimeout(() => {
+              that.snackBar.open('Auto mapping of employees may take up to 10 minutes');
+            }, 1500);
+          }
           that.netsuite.getGeneralSettings();
           that.router.navigateByUrl(`workspaces/${that.workspaceId}/dashboard`);
         });
