b1342340d37f4fd04efe31ba9c7ffcffd8efbd09
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2021-04-07 12:57:29 +0530

    Restrict from triggering export when exports are still ongoing (#57)

--- src/app/netsuite/sync-export/export/export.component.ts
+++ src/app/netsuite/sync-export/export/export.component.ts
@@ -12,6 +12,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { SettingsService } from 'src/app/core/services/settings.service';
 import { WindowReferenceService } from 'src/app/core/services/window.service';
 import { GeneralSetting } from 'src/app/core/models/general-setting.model';
+import { TaskResponse } from 'src/app/core/models/task-reponse.model';
 
 @Component({
   selector: 'app-export',
@@ -22,6 +23,8 @@ export class ExportComponent implements OnInit {
 
   isLoading: boolean;
   isExporting: boolean;
+  isProcessingExports: boolean;
+  processingExportsCount: number;
   workspaceId: number;
   exportableExpenseGroups: ExpenseGroup[];
   generalSettings: GeneralSetting;
@@ -147,15 +150,49 @@ export class ExportComponent implements OnInit {
     });
   }
 
+  filterOngoingTasks(tasks: TaskResponse) {
+    return tasks.results.filter(task => (task.status === 'IN_PROGRESS' || task.status === 'ENQUEUED') && task.type !== 'FETCHING_EXPENSES').length;
+  }
 
-  ngOnInit() {
+  checkOngoingExports() {
+    const that = this;
+
+    that.isProcessingExports = true;
+    interval(7000).pipe(
+      switchMap(() => from(that.taskService.getAllTasks('ALL'))),
+      takeWhile((response: TaskResponse) => that.filterOngoingTasks(response) > 0, true)
+    ).subscribe((tasks: TaskResponse) => {
+      that.processingExportsCount = that.filterOngoingTasks(tasks);
+      if (that.filterOngoingTasks(tasks) === 0) {
+        that.isProcessingExports = false;
+        that.loadExportableExpenseGroups();
+        that.snackBar.open('Export Complete');
+      }
+    });
+  }
+
+  reset() {
     const that = this;
 
     that.isExporting = false;
+    that.isLoading = true;
+
+    that.taskService.getAllTasks('ALL').subscribe((tasks: TaskResponse) => {
+      that.isLoading = false;
+      if (that.filterOngoingTasks(tasks) === 0) {
+        that.loadExportableExpenseGroups();
+      } else {
+        that.processingExportsCount = that.filterOngoingTasks(tasks);
+        that.checkOngoingExports();
+      }
+    });
+  }
+
+  ngOnInit() {
+    const that = this;
     that.workspaceId = +that.route.parent.snapshot.params.workspace_id;
 
-    that.isLoading = true;
-    that.loadExportableExpenseGroups();
+    that.reset();
   }
 
 }
