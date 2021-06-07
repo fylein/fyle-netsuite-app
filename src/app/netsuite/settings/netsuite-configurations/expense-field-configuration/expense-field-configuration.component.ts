683326b57bbcd6e9f5805dfc6a3f733c26bf501f
ashwinthanaraj <ashwin.t@fyle.in>
2021-06-07 11:34:22 +0530

    Import NetSuite fields as Select type Custom Field in Fyle

--- src/app/netsuite/settings/netsuite-configurations/expense-field-configuration/expense-field-configuration.component.ts
+++ src/app/netsuite/settings/netsuite-configurations/expense-field-configuration/expense-field-configuration.component.ts
@@ -9,6 +9,8 @@ import { WindowReferenceService } from 'src/app/core/services/window.service';
 import { NetSuiteComponent } from 'src/app/netsuite/netsuite.component';
 import { MappingSetting } from 'src/app/core/models/mapping-setting.model';
 import { ExpenseField } from 'src/app/core/models/expense-field.model';
+import { MatSnackBar } from '@angular/material';
+import { MappingSettingResponse } from 'src/app/core/models/mapping-setting-response.model';
 
 @Component({
   selector: 'app-expense-field-configuration',
@@ -17,6 +19,7 @@ import { ExpenseField } from 'src/app/core/models/expense-field.model';
 })
 export class ExpenseFieldConfigurationComponent implements OnInit {
   expenseFieldsForm: FormGroup;
+  customFieldForm: FormGroup;
   expenseFields: FormArray;
   workspaceId: number;
   isLoading: boolean;
@@ -27,22 +30,32 @@ export class ExpenseFieldConfigurationComponent implements OnInit {
   selectedFyleFields: string[] = [];
   netsuiteFormFieldList: ExpenseField[];
   windowReference: Window;
+  showCustomFieldName: boolean;
+  customFieldName = 'Add custom field';
+  isSystemField: boolean;
 
-  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute, private router: Router, private settingsService: SettingsService, private mappingsService: MappingsService, private netsuite: NetSuiteComponent, private windowReferenceService: WindowReferenceService) {
+  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute, private router: Router, private settingsService: SettingsService, private mappingsService: MappingsService, private snackBar: MatSnackBar, private netsuite: NetSuiteComponent, private windowReferenceService: WindowReferenceService) {
     this.windowReference = this.windowReferenceService.nativeWindow;
-   }
+  }
 
-  createExpenseField(sourceField: string = '', destinationField: string = '') {
+  createExpenseField(sourceField: string = '', destinationField: string = '', importToFyle: boolean = false) {
     const that = this;
 
     const group = that.formBuilder.group({
       source_field: [sourceField ? sourceField : '', [Validators.required, RxwebValidators.unique()]],
       destination_field: [destinationField ? destinationField : '', [Validators.required, RxwebValidators.unique()]],
+      import_to_fyle: [importToFyle]
     });
 
     if (sourceField && destinationField) {
-      group.disable();
+      group.controls.source_field.disable();
+      group.controls.destination_field.disable();
     }
+
+    // Save mapping setting on toggle change
+    group.controls.import_to_fyle.valueChanges.subscribe(() => {
+      that.saveExpenseFields();
+    });
     return group;
   }
 
@@ -56,6 +69,7 @@ export class ExpenseFieldConfigurationComponent implements OnInit {
 
   addExpenseField() {
     const that = this;
+    that.hideCustomField();
 
     that.expenseFields = that.expenseFieldsForm.get('expenseFields') as FormArray;
     that.expenseFields.push(that.createExpenseField());
@@ -64,14 +78,36 @@ export class ExpenseFieldConfigurationComponent implements OnInit {
   saveExpenseFields() {
     const that = this;
 
-    that.isLoading = true;
-    const expenseFields: MappingSetting[] = that.expenseFieldsForm.value.expenseFields;
+    if (that.expenseFieldsForm.valid) {
+      that.isLoading = true;
+      let isCustomField = false;
+      // getRawValue() would have values even if they are disabled
+      const expenseFields: MappingSetting[] = that.expenseFieldsForm.getRawValue().expenseFields;
+      expenseFields.forEach(element => {
+        if (element.source_field === 'custom') {
+          element.source_field = this.customFieldForm.value.customFieldName;
+          element.is_custom = true;
+          element.import_to_fyle = true;
+          isCustomField = true;
+        } else {
+          element.is_custom = false;
+        }
+      });
 
-    that.settingsService.postMappingSettings(that.workspaceId, expenseFields).subscribe(response => {
-      that.netsuite.getGeneralSettings();
-      that.router.navigateByUrl(`/workspaces/${that.workspaceId}/dashboard`);
-      that.isLoading = false;
-    });
+      that.settingsService.postMappingSettings(that.workspaceId, expenseFields).subscribe((mappingSetting: MappingSetting[]) => {
+        that.netsuite.refreshDashboardMappingSettings(mappingSetting);
+        that.createFormFields(mappingSetting);
+        if (isCustomField) {
+          that.getFyleFields().then(() => {
+            that.isLoading = false;
+          });
+        } else {
+          that.isLoading = false;
+        }
+      }, () => that.snackBar.open('Something went wrong while saving expense fields mapping'));
+    } else {
+      that.snackBar.open('Please fill all mandatory fields');
+    }
   }
 
   removeExpenseField(index: number) {
@@ -81,41 +117,103 @@ export class ExpenseFieldConfigurationComponent implements OnInit {
     expenseFields.removeAt(index);
   }
 
+  showCustomField() {
+    this.showCustomFieldName = true;
+    this.customFieldForm.markAllAsTouched();
+  }
+
+  updateCustomFieldName(name: string) {
+    const that = this;
+
+    let existingFields: string[] = that.fyleExpenseFields.map(fields => fields.display_name.toLowerCase());
+    const systemFields = ['employee id', 'organisation name', 'employee name', 'employee email', 'expense date', 'expense date', 'expense id', 'report id', 'employee id', 'department', 'state', 'reporter', 'report', 'purpose', 'vendor', 'category', 'category code', 'mileage distance', 'mileage unit', 'flight from city', 'flight to city', 'flight from date', 'flight to date', 'flight from class', 'flight to class', 'hotel checkin', 'hotel checkout', 'hotel location', 'hotel breakfast', 'currency', 'amount', 'foreign currency', 'foreign amount', 'tax', 'approver', 'project', 'billable', 'cost center', 'cost center code', 'approved on', 'reimbursable', 'receipts', 'paid date', 'expense created date'];
+    existingFields = existingFields.concat(systemFields);
+
+    if (existingFields.indexOf(name.toLowerCase()) !== -1) {
+      that.isSystemField = true;
+    } else {
+      that.isSystemField = false;
+    }
+    this.customFieldName = name;
+  }
+
+  hideCustomField() {
+    this.showCustomFieldName = false;
+  }
+
+  saveCustomField() {
+    this.showCustomFieldName = false;
+    this.saveExpenseFields();
+  }
+
+  createFormFields(mappingSetting: MappingSetting[]) {
+    const that = this;
+
+    that.mappingSettings = mappingSetting.filter(
+      setting => setting.source_field !== 'EMPLOYEE' && setting.source_field !== 'CATEGORY'
+    );
+
+    let expenseFieldFormArray;
+    if (that.mappingSettings.length) {
+      expenseFieldFormArray = that.mappingSettings.map(
+        setting => that.createExpenseField(setting.source_field, setting.destination_field, setting.import_to_fyle)
+      );
+    } else {
+      expenseFieldFormArray = [that.createExpenseField()];
+    }
+
+    that.expenseFieldsForm = that.formBuilder.group({
+      expenseFields: that.formBuilder.array(expenseFieldFormArray)
+    });
+  }
+
+  getMappingSettings() {
+    const that = this;
+
+    return that.settingsService.getMappingSettings(that.workspaceId).toPromise().then((mappingSetting: MappingSettingResponse) => {
+      that.createFormFields(mappingSetting.results);
+
+      return mappingSetting;
+    });
+  }
+
+  getFyleFields() {
+    const that = this;
+
+    return that.mappingsService.getFyleExpenseAttributes().toPromise().then((fyleFields: ExpenseField[]) => {
+      that.fyleExpenseFields = fyleFields;
+      that.fyleFormFieldList = fyleFields;
+
+      return fyleFields;
+    });
+  }
+
+  getNetSuiteFields() {
+    const that = this;
+
+    return that.mappingsService.getNetSuiteFields().toPromise().then((netsuiteFields: ExpenseField[]) => {
+      that.netsuiteFields = netsuiteFields;
+      that.netsuiteFormFieldList = netsuiteFields;
+
+      return netsuiteFields;
+    });
+  }
+
   getSettings() {
     const that = this;
-    forkJoin(
-      [
-        that.settingsService.getMappingSettings(that.workspaceId),
-        that.mappingsService.getFyleExpenseAttributes(),
-        that.mappingsService.getNetSuiteFields()
-      ]
-    ).subscribe(response => {
-      that.mappingSettings = response[0].results.filter(
-        setting => setting.source_field !== 'EMPLOYEE' && setting.source_field !== 'CATEGORY'
-      );
 
-      let expenseFieldFormArray;
-
-      that.fyleExpenseFields = response[1];
-      that.netsuiteFields = response[2];
-
-      that.fyleFormFieldList = response[1];
-      that.netsuiteFormFieldList = response[2];
-
-      if (that.mappingSettings.length) {
-        expenseFieldFormArray = that.mappingSettings.map(
-          setting => that.createExpenseField(setting.source_field, setting.destination_field)
-        );
-      } else {
-        expenseFieldFormArray = [that.createExpenseField()];
-      }
-
-      that.expenseFieldsForm = that.formBuilder.group({
-        expenseFields: that.formBuilder.array(expenseFieldFormArray)
-      });
-
-      that.isLoading = false;
+    that.customFieldForm = that.formBuilder.group({
+      customFieldName: ['', Validators.required]
     });
+
+    that.getMappingSettings()
+      .then(() => {
+        return that.getFyleFields();
+      }).then(() => {
+        return that.getNetSuiteFields();
+      }).finally(() => {
+        that.isLoading = false;
+      });
   }
 
   ngOnInit() {
