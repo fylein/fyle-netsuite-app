d840398820b89e54235f9f52a8928d293de3631b
Nilesh Pant <nileshpant6969@gmail.com>
2021-08-05 18:43:26 +0530

    content update

--- src/app/netsuite/sync-export/sync/sync.component.ts
+++ src/app/netsuite/sync-export/sync/sync.component.ts
@@ -57,7 +57,7 @@ export class SyncComponent implements OnInit {
             that.snackBar.open('Import Complete');
           } else {
             const expenseState = that.expenseGroupSettings.expense_state.toLowerCase().replace('_', ' ');
-            that.snackBar.open(`No new expense group imported kindly check your fyle account to see if there are any expenses in the ${expenseState} state`, null, {
+            that.snackBar.open(`No new expense groups were imported. Kindly check your Fyle account to see if there are any expenses in the ${expenseState} state`, null, {
               duration: 5000
             });
           }
