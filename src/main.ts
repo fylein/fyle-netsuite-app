d11c5ec1eaa241b45b49b9bc62fd5ce05f7d7a25
labhvam5 <labhvamkrsharma@gmail.com>
2021-08-11 18:05:55 +0530

    added the hotjar integration

--- src/main.ts
+++ src/main.ts
@@ -11,3 +11,17 @@ if (environment.production) {
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+if (environment.app_url === 'https://netsuite.fyleapps.tech') {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.innerHTML = `
+    (function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:2548028,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+  document.getElementsByTagName('head')[0].appendChild(script);
+}
