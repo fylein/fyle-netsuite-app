e5ebfe36059f287bc87d8218e0009dd143bd8147
labhvam5 <88420539+labhvam5@users.noreply.github.com>
2021-08-12 12:43:59 +0530

    added the hotjar integration (#84)
    added the hotjar integration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,7 @@ export const environment = {
   fyle_client_id: '{{FYLE_CLIENT_ID}}',
   callback_uri: '{{CALLBACK_URI}}',
   api_url: '{{API_URL}}',
-  app_url: '{{APP_URL}}'
+  app_url: '{{APP_URL}}',
+  hotjar_whitelist_url: '{{HOTJAR_WHITELIST_URL}}',
+  hotjar_id: '{{HOTJAR_ID}}'
 };
