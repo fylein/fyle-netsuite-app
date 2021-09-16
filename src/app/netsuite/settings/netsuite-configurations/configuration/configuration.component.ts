6ff34e6b2a8bd946887b57cecc8b44869fea5d32
ashwinthanaraj <ashwin.t@fyle.in>
2021-09-16 19:30:58 +0530

    Reset ccc field when reimburse field is changed

--- src/app/netsuite/settings/netsuite-configurations/configuration/configuration.component.ts
+++ src/app/netsuite/settings/netsuite-configurations/configuration/configuration.component.ts
@@ -154,6 +154,8 @@ export class ConfigurationComponent implements OnInit {
           // turn off the import categories toggle when the user switches from EXPENSE REPORT to something else
           that.generalSettingsForm.controls.importCategories.setValue(false);
         }
+      } else {
+        that.generalSettingsForm.controls.cccExpense.reset();
       }
 
       if (that.generalSettings && that.generalSettings.sync_fyle_to_netsuite_payments && !that.showPaymentsandProjectsField) {
