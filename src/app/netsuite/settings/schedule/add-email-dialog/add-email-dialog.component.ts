5ef2e7d79152897570109f09dcf56b7ca45cb416
Nilesh Pant <nileshpant6969@gmail.com>
2022-03-07 23:40:01 +0530

    minor changes

--- src/app/netsuite/settings/schedule/add-email-dialog/add-email-dialog.component.ts
+++ src/app/netsuite/settings/schedule/add-email-dialog/add-email-dialog.component.ts
@@ -30,7 +30,7 @@ export class AddEmailDialogComponent implements OnInit {
       email: that.form.value.adminEmail
     };
 
-    that.settingsService.postSettings(that.data.hours, that.data.schedule_enabled, null, adminData).subscribe(() => {
+    that.settingsService.postSettings(that.data.hours, that.data.schedulEnabled, that.data.selectedEmails, adminData).subscribe(() => {
       that.isLoading = false;
       that.dialogRef.close();
     });
