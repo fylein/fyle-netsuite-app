426a536583edde77ed6dd3e5f008ca31b111ef59
Dhaarani <55541808+DhaaraniCIT@users.noreply.github.com>
2022-03-15 13:12:54 +0530

    If category mapping is missing, when clicked on that, open the category mapping dialog with pre filled fyle value (#129)
    If category mapping is missing, when clicked on that, open the category mapping dialog with pre filled fyle value

--- src/app/netsuite/expense-groups/view-expense-group/group-mapping-error/group-mapping-error.component.ts
+++ src/app/netsuite/expense-groups/view-expense-group/group-mapping-error/group-mapping-error.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
 import { TasksService } from 'src/app/core/services/tasks.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Task } from 'src/app/core/models/task.model';
 import { MappingError } from 'src/app/core/models/mapping-error.model';
+import { CategoryMapping } from 'src/app/core/models/category-mapping.model';
+import { CategoryMappingsDialogComponent } from '../../../settings/category-mappings/category-mappings-dialog/category-mappings-dialog.component';
+import { StorageService } from 'src/app/core/services/storage.service';
+import { MappingsService } from 'src/app/core/services/mappings.service';
+import { CategoryMappingsResponse } from 'src/app/core/models/category-mapping-response.model';
 
 @Component({
   selector: 'app-group-mapping-error',
@@ -16,10 +22,17 @@ export class GroupMappingErrorComponent implements OnInit {
   expenseGroupId: number;
   workspaceId: number;
 
+  categoryMappings: CategoryMapping[];
   mappingErrors: MatTableDataSource<MappingError> = new MatTableDataSource([]);
   columnsToDisplay = ['category', 'message', 'type'];
 
-  constructor(private taskService: TasksService, private route: ActivatedRoute) { }
+  constructor(
+    private taskService: TasksService,
+    private route: ActivatedRoute,
+    private storageService: StorageService,
+    public dialog: MatDialog,
+    private router: Router,
+    private mappingsService: MappingsService) { }
 
   ngOnInit() {
     const that = this;
@@ -31,4 +44,20 @@ export class GroupMappingErrorComponent implements OnInit {
       that.isLoading = false;
     });
   }
+
+  open(selectedItem) {
+    const that = this;
+    if (selectedItem.type === 'Category Mapping' ) {
+
+      const dialogRef = that.dialog.open(CategoryMappingsDialogComponent, {
+        width: '450px',
+        data: {
+          workspaceId: that.workspaceId,
+          categoryMappingRow: selectedItem,
+          category: selectedItem.value
+        }
+      });
+    }
+  }
+
 }
