add27f39716522f1eb9579f11a583f7eb71241a4
anishfyle <anishkumar.s@fyle.in>
2023-01-02 10:21:47 +0530

    code refactor 2

--- src/app/netsuite/settings/netsuite-configurations/skip-export/skip-export.component.ts
+++ src/app/netsuite/settings/netsuite-configurations/skip-export/skip-export.component.ts
@@ -12,13 +12,12 @@ export class SkipExportComponent implements OnInit {
   excludeForm: FormGroup;
   excludeForm1: FormGroup;
   condition: Boolean;
-  operator_field_final: { id: number,label: string, value: string }[];
+  operator_field: { id: number,label: string, value: string }[];
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.condition = false;
     this.initFormGroups();
-    this.initFormGroups1();
   }
 
   checkCondition() {
@@ -38,17 +37,18 @@ export class SkipExportComponent implements OnInit {
   onChange(event)
   {
     console.log(event['value']);
-    this.operator_field_final = this.getConditionField(event['value']);
+    this.operator_field = this.getConditionField(event['value']);
   }
 
   conditional_field = [
     {value: "EXPENSE CUSTOM FIELDS", label: "Expense Custom Fields" },
-    {value: "REPORT NAME", label: "Report Number" },
+    {value: "REPORT NUMBER", label: "Report Number" },
     {value: "EMPLOYEE EMAIL", label: "Employee Email" },
-    {value: "DATE OF SPEND", label: "Date of Spend" }
+    {value: "DATE OF SPEND", label: "Date of Spend" },
+    {value: "REPORT NAME", label: "Report Name" }
   ];
 
-  operator_field = [
+  operator_field_temp = [
     {value: "EXPENSE CUSTOM FIELDS", label: "is empty" },
     {value: "EXPENSE CUSTOM FIELDS", label: "is not empty" },
     {value: "EXPENSE CUSTOM FIELDS", label: "is equal" },
@@ -62,7 +62,7 @@ export class SkipExportComponent implements OnInit {
 
     if (conditionField === 'EXPENSE CUSTOM FIELDS') {
       operatorList.push({
-        label: 'is equal',
+        label: 'is empty',
         value: 'EXPENSE CUSTOM FIELDS'
       });
       operatorList.push({
@@ -70,8 +70,18 @@ export class SkipExportComponent implements OnInit {
         value: 'EXPENSE CUSTOM FIELDS'
       });
       operatorList.push({
-        label: 'is empty',
+        label: 'is equal',
         value: 'EXPENSE CUSTOM FIELDS'
+      });
+    }
+    else if(conditionField === 'REPORT NUMBER'){
+      operatorList.push({
+        value: "REPORT NUMBER", label: "is equal"
+      });
+    }
+    else if(conditionField === 'EMPLOYEE EMAIL'){
+      operatorList.push({
+        value: "EMPLOYEE EMAIL", label: "is equal"
       });
     }
     else if(conditionField === 'DATE OF SPEND'){
@@ -82,11 +92,20 @@ export class SkipExportComponent implements OnInit {
         value: "DATE OF SPEND", label: "is it on or before" 
       });
     }
+    else if(conditionField === 'REPORT NAME'){
+      operatorList.push({
+        value: "REPORT NAME", label: "Contains"
+      });
+      operatorList.push({
+        value: "REPORT NAME", label: "is equal" 
+      });
+    }
     return {
       'EXPENSE CUSTOM FIELDS': operatorList,
+      'REPORT NUMBER': operatorList,
+      'EMPLOYEE EMAIL': operatorList,
       'DATE OF SPEND': operatorList,
-      'REPORT NAME': operatorList,
-      'EMPLOYEE EMAIL': operatorList
+      'REPORT NAME': operatorList
     }[conditionField];
   }
 
@@ -99,18 +118,14 @@ export class SkipExportComponent implements OnInit {
   ];
   
   initFormGroups() {
+    
     this.excludeForm = new FormGroup({
       condition: new FormControl("", [Validators.required]),
       operator: new FormControl("", [Validators.required]),
       value: new FormControl("", [Validators.required]),
-    });
-  }
-
-  initFormGroups1() {
-    this.excludeForm1 = new FormGroup({
-      condition: new FormControl("", [Validators.required]),
-      operator: new FormControl("", [Validators.required]),
-      value: new FormControl("", [Validators.required]),
+      condition_1: new FormControl("", [Validators.required]),
+      operator_1: new FormControl("", [Validators.required]),
+      value_1: new FormControl("", [Validators.required])
     });
   }
 }
