28fd6f3fd91003780f91cfc119cc01609d68e292
anishfyle <anishkumar.s@fyle.in>
2023-01-02 08:35:39 +0530

    added conditional options for operator dropdown menu

--- src/app/netsuite/settings/netsuite-configurations/skip-export/skip-export.component.ts
+++ src/app/netsuite/settings/netsuite-configurations/skip-export/skip-export.component.ts
@@ -1,75 +1,116 @@
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { RxwebValidators } from '@rxweb/reactive-form-validators';
+import { Component, OnChanges, OnInit } from "@angular/core";
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import { RxwebValidators } from "@rxweb/reactive-form-validators";
+import { event } from "cypress/types/jquery";
 
 @Component({
-  selector: 'app-skip-export',
-  templateUrl: './skip-export.component.html',
-  styleUrls: ['./skip-export.component.scss']
+  selector: "app-skip-export",
+  templateUrl: "./skip-export.component.html",
+  styleUrls: ["./skip-export.component.scss"],
 })
 export class SkipExportComponent implements OnInit {
   excludeForm: FormGroup;
   excludeForm1: FormGroup;
   condition: Boolean;
-  constructor(private formBuilder: FormBuilder) { }
+  operator_field_final: { id: number,label: string, value: string }[];
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
-    this.condition = false
+    this.condition = false;
     this.initFormGroups();
     this.initFormGroups1();
   }
 
-  checkCondition()
-  {
-    return this.condition
+  checkCondition() {
+    return this.condition;
   }
 
   addCondition() {
-    this.condition = true
+    this.condition = true;
     this.excludeForm1.enable();
   }
 
-  remCondition(){
-    this.condition = false
+  remCondition() {
+    this.condition = false;
     this.excludeForm1.disable();
   }
 
- conditional_field = [
-      {id: 1, text: 'Expense Custom Fields'},
-      {id: 2, text: 'Report Number'},
-      {id: 3, text: 'Employee Email'},
-      {id: 4, text: 'Date of Spend'},
-      {id: 5, text: 'Report Name'}
+  onChange(event)
+  {
+    console.log(event['value']);
+    this.operator_field_final = this.getConditionField(event['value']);
+  }
+
+  conditional_field = [
+    {value: "EXPENSE CUSTOM FIELDS", label: "Expense Custom Fields" },
+    {value: "REPORT NAME", label: "Report Number" },
+    {value: "EMPLOYEE EMAIL", label: "Employee Email" },
+    {value: "DATE OF SPEND", label: "Date of Spend" }
   ];
 
- operator_field = [
-    {id: 1, text: 'is empty'},
-    {id: 2, text: 'is not empty'},
-    {id: 3, text: 'is equal'},
-    {id: 4, text: 'Contains'},
-    {id: 5, text: 'is it on or before'}
-];
+  operator_field = [
+    {value: "EXPENSE CUSTOM FIELDS", label: "is empty" },
+    {value: "EXPENSE CUSTOM FIELDS", label: "is not empty" },
+    {value: "EXPENSE CUSTOM FIELDS", label: "is equal" },
+    {value: "REPORT NAME", label: "Contains" },
+    {value: "DATE OF SPEND", label: "is Before" },
+    {value: "DATE OF SPEND", label: "is it on or before" },
+  ];
 
-value_field = [
-  {id: 1, text: 'ajdnwjnadw'},
-  {id: 2, text: 'djanwjnadw'},
-  {id: 3, text: 'wxdnwjnadw'},
-  {id: 4, text: 'nvdnwjnadw'},
-  {id: 5, text: 'npxnwjnadw'}
-];
-  initFormGroups(){
+  getConditionField(conditionField: string) {
+    const operatorList = [];
+
+    if (conditionField === 'EXPENSE CUSTOM FIELDS') {
+      operatorList.push({
+        label: 'is equal',
+        value: 'EXPENSE CUSTOM FIELDS'
+      });
+      operatorList.push({
+        label: 'is not empty',
+        value: 'EXPENSE CUSTOM FIELDS'
+      });
+      operatorList.push({
+        label: 'is empty',
+        value: 'EXPENSE CUSTOM FIELDS'
+      });
+    }
+    else if(conditionField === 'DATE OF SPEND'){
+      operatorList.push({
+        value: "DATE OF SPEND", label: "is Before"
+      });
+      operatorList.push({
+        value: "DATE OF SPEND", label: "is it on or before" 
+      });
+    }
+    return {
+      'EXPENSE CUSTOM FIELDS': operatorList,
+      'DATE OF SPEND': operatorList,
+      'REPORT NAME': operatorList,
+      'EMPLOYEE EMAIL': operatorList
+    }[conditionField];
+  }
+
+  value_field = [
+    { label: "ajdnwjnadw" },
+    { label: "djanwjnadw" },
+    { label: "wxdnwjnadw" },
+    { label: "nvdnwjnadw" },
+    { label: "npxnwjnadw" },
+  ];
+  
+  initFormGroups() {
     this.excludeForm = new FormGroup({
-      condition: new FormControl('',[Validators.required]), 
-      operator: new FormControl('',[Validators.required]), 
-      value: new FormControl('',[Validators.required])
+      condition: new FormControl("", [Validators.required]),
+      operator: new FormControl("", [Validators.required]),
+      value: new FormControl("", [Validators.required]),
     });
   }
 
-  initFormGroups1(){
+  initFormGroups1() {
     this.excludeForm1 = new FormGroup({
-      condition: new FormControl('',[Validators.required]), 
-      operator: new FormControl('',[Validators.required]), 
-      value: new FormControl('',[Validators.required])
+      condition: new FormControl("", [Validators.required]),
+      operator: new FormControl("", [Validators.required]),
+      value: new FormControl("", [Validators.required]),
     });
   }
 }
