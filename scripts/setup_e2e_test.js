a121cdf60bb0632ea5cb7bfcc22176a42ef78b29
Fyle <fyle@Fyles-MacBook-Air.local>
2023-01-04 18:33:09 +0530

    setup e2e test changes 3

--- scripts/setup_e2e_test.js
+++ scripts/setup_e2e_test.js
@@ -64,38 +64,38 @@ readFile('./src/environments/environment.json', 'utf8', (err, data) => {
     }
   }).end();
 
-  http.request(options, function(res) {
-    if (res.statusCode === 200) {
-      console.log('Prepared workspace for e2e tests');
+  // http.request(options, function(res) {
+  //   if (res.statusCode === 200) {
+  //     console.log('Prepared workspace for e2e tests');
 
-      options.path = '/api/auth/refresh/';
-      const payload = JSON.stringify({refresh_token: environment.e2e_tests.secret[1].refresh_token});
-      options.headers['Content-Length'] = Buffer.byteLength(payload)
+  //     options.path = '/api/auth/refresh/';
+  //     const payload = JSON.stringify({refresh_token: environment.e2e_tests.secret[1].refresh_token});
+  //     options.headers['Content-Length'] = Buffer.byteLength(payload)
 
-      const request = http.request(options, function(response) {
-        let body = '';
-        response.on('data', (chunk) => {
-          body += chunk;
-        });
-        response.on('end', () => {
-          const token = JSON.parse(body);
-          environment.e2e_tests.secret[1].access_token = token.access_token;
+  //     const request = http.request(options, function(response) {
+  //       let body = '';
+  //       response.on('data', (chunk) => {
+  //         body += chunk;
+  //       });
+  //       response.on('end', () => {
+  //         const token = JSON.parse(body);
+  //         environment.e2e_tests.secret[1].access_token = token.access_token;
 
-          const targetPath = './src/environments/environment.json';
-          writeFile(targetPath, JSON.stringify(environment), 'utf8', (err) => {
-            if (err) {
-              return console.error(err);
-            }
+  //         const targetPath = './src/environments/environment.json';
+  //         writeFile(targetPath, JSON.stringify(environment), 'utf8', (err) => {
+  //           if (err) {
+  //             return console.error(err);
+  //           }
 
-            console.log('Updated access token');
-          });
-        });
-      });
-      request.write(payload);
-      request.end();
-    } else {
-      console.log('Error preparing workspace for e2e tests', res.statusMessage);
-      throw res.statusMessage;
-    }
-  }).end();
+  //           console.log('Updated access token');
+  //         });
+  //       });
+  //     });
+  //     request.write(payload);
+  //     request.end();
+  //   } else {
+  //     console.log('Error preparing workspace for e2e tests', res.statusMessage);
+  //     throw res.statusMessage;
+  //   }
+  // }).end();
 });
