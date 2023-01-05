37855faa50649e2b1497db8505aa872f941844b3
Fyle <fyle@Fyles-MacBook-Air.local>
2023-01-05 14:52:54 +0530

    setup e2e changes 2

--- scripts/setup_e2e_test.js
+++ scripts/setup_e2e_test.js
@@ -14,7 +14,7 @@ readFile('./src/environments/environment.json', 'utf8', (err, data) => {
     host = 'localhost';
   } else {
     http = require('https');
-    console.log("wqwq",baseUrl.split('://'),baseUrl)
+    console.log("wqwq",baseUrl.split('://'),baseUrl, environment.app_url)
     host = baseUrl.split('://')[1].replace('/api', '');
   }
 
