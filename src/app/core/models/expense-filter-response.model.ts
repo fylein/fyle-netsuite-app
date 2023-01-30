fddba6eaed51aaddeb0833baa35d9983aa94e00d
anishfyle <anishkumar.s@fyle.in>
2023-01-30 06:20:14 +0530

    formatterFix

--- src/app/core/models/expense-filter-response.model.ts
+++ src/app/core/models/expense-filter-response.model.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 
-import { SkipExport } from "./skip-export.model";
+import { SkipExport } from './skip-export.model';
 
 export type ExpenseFilterResponse = {
   count: number;
