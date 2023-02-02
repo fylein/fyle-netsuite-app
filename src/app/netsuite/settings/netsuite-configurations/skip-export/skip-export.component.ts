34460c168ce391868800b92ca9b5e0e2d58f85c8
anishfyle <anishkumar.s@fyle.in>
2023-02-03 05:11:50 +0530

    Is_not (not_in) for SELECT Type

--- src/app/netsuite/settings/netsuite-configurations/skip-export/skip-export.component.ts
+++ src/app/netsuite/settings/netsuite-configurations/skip-export/skip-export.component.ts
@@ -74,6 +74,10 @@ export class SkipExportComponent implements OnInit {
     {
       label: 'Is',
       value: 'iexact',
+    },
+    {
+      label: 'Is not',
+      value: 'not_in'
     }
   ];
   valueOption1: any[] = [];
@@ -419,6 +423,7 @@ export class SkipExportComponent implements OnInit {
         } else {
           this.isDisabledChip1 = false;
         }
+        console.log(operatorSelected);
       }
     );
 
