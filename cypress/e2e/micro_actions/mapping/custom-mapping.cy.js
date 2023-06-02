ebb673c4b515f30b8800f8d6ff6383270910ad55
Fyle <fyle@Fyles-MacBook-Air.local>
2023-06-02 14:59:40 +0530

    e2e assert fix for mapping

--- cypress/e2e/micro_actions/mapping/custom-mapping.cy.js
+++ cypress/e2e/micro_actions/mapping/custom-mapping.cy.js
@@ -15,7 +15,7 @@ describe('Cost Center Mapping', () => {
         cy.getElement('save-btn').click()
     }
 
-    it('Pagination test', () => {
+    it('Update cost center mapping', () => {
         cy.get('.page-header--name').contains('Cost Center Mappings')
         cy.url().should('include', '/settings/cost_center/mappings')
         cy.get('button').first().contains('New Cost Center Mapping')
@@ -26,8 +26,8 @@ describe('Cost Center Mapping', () => {
         cy.getElement('custom-netsuite-value').get('input').eq(2).then(($input) => {
             const value = $input[0].value
             cy.getElement('custom-netsuite-value').get('input').eq(2).clear()
-            cy.getElement('custom-netsuite-value').get('input').eq(2).type('Ashu')
-            cy.getElement('custom-netsuite-value').get('mat-autocomplete').get('mat-option').eq(0).click()
+            cy.getElement('custom-netsuite-value').get('input').eq(2).type('fyle')
+            cy.getElement('custom-netsuite-value').get('mat-option').eq(0).click()
             cy.assertText('save-btn','Save')
             cy.getElement('save-btn').click()
             cy.getElement('custom-table-netsuite-value').eq(0).contains('Ashu')
